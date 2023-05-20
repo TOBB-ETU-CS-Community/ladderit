08f5444b654797310fbe0da9d00e7f8c2dd5ac69
Recep Cankaya <recepcnky@outlook.com>
2023-05-21 00:37:52 +0300

    add navbar

---
+++ ladderit-frontend/components/Navbar.jsx
@@ -0,0 +1,15 @@
+"use client";
+import Link from "next/link";
+import { ConnectWallet } from "@thirdweb-dev/react";
+
+export default function Navbar() {
+  return (
+    <div className="w-full h-20 flex justify-evenly items-center bg-[#6A666C] text-bgColor">
+      <Link href="/" className="text-3xl">
+        LADDERIT
+      </Link>
+      <Link href="/">Register</Link>
+      <ConnectWallet className="grow-0" />
+    </div>
+  );
+}
