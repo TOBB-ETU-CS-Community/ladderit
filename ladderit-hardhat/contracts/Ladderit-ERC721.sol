8db4b563289126aa26339159ca666cd1cbcf3cff
Cyberchain23 <cybercagri@gmail.com>
2023-05-21 00:49:33 +0300

    Task and ERC721 added

---
+++ ladderit-hardhat/contracts/Ladderit-ERC721.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MyNFT is ERC721 {
+    constructor() ERC721("LadderIt", "LADIT") {}
+
+    function mint(address to, uint256 tokenId) public {
+        _safeMint(to, tokenId);
+    }
+}
