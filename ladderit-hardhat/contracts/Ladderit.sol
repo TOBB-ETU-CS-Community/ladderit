259225b64403bd9e896aab89b5fa4d57503772b2
Recep Cankaya <recepcnky@outlook.com>
2023-05-21 09:58:09 +0300

    finish user registration and some tasks operations

--- ladderit-hardhat/contracts/Ladderit.sol
+++ ladderit-hardhat/contracts/Ladderit.sol
@@ -2,65 +2,65 @@
 pragma solidity ^0.8.17;
 
 contract Ladderit {
-
     struct User {
-
         address client;
-        string  name;
+        string name;
+        string[] tasks;
+        mapping(uint256 => bool) dailyTasks;
     }
 
-    struct Task {
-        string description;
+    mapping(address => User) public users;
+    mapping(string => bool) public isTaken;
+
+    event TaskCompleted(uint256 indexed taskID);
+    event UserName(string indexed name);
+
+    function addTask(string calldata _task) external {
+        require(
+            msg.sender == users[msg.sender].client,
+            "You have to register first"
+        );
+        User storage user = users[msg.sender];
+        require(
+            users[msg.sender].tasks.length < 5,
+            "Maximum task limit reached"
+        );
+        user.tasks.push(_task);
     }
 
-    mapping(uint256 => bool) private dailyTasks;
+    function getTasks() public view returns (string[] memory) {
+        return users[msg.sender].tasks;
+    }
 
-    event TaskCompleted (uint256 indexed taskID);
+    function delTask(uint256 taskIndex) external returns (string memory) {
+        User storage user = users[msg.sender];
+        require(taskIndex <= user.tasks.length, "Invalid task index");
+        require(user.client == msg.sender, "You are not allowed");
+        string memory selectedTask = user.tasks[taskIndex];
+        user.tasks[taskIndex] = "";
+        return selectedTask;
+    }
 
-    mapping (address => User) public users;
-
-    mapping (string => bool) public isTaken;
-
-    event UserName (string indexed name);
-    
-    
-    mapping(address => Task[]) public userTasks;
-
-    function getUserName(string memory _name) public {
+    function getUserName(string calldata _name) external {
         User storage user = users[msg.sender];
         require(!isTaken[_name], "Name is already taken");
+        require(bytes(user.name).length == 0, "User already registered");
         user.name = _name;
         user.client = msg.sender;
-        require(bytes(users[msg.sender].name).length > 0, "Name cannot be empty");
         isTaken[_name] = true;
+
         emit UserName(user.name);
     }
-    
-    function addTask(string memory _description) public {
-        require(userTasks[msg.sender].length < 5, "Maximum task limit reached");
-        Task memory newTask = Task(_description);
-        userTasks[msg.sender].push(newTask);
-    }
 
-    function getTasks() public view returns (Task[] memory) {
-        return userTasks[msg.sender];
-    }
-    
-    function delTask(uint256 taskIndex) public {
-        require(taskIndex < userTasks[msg.sender].length, "Invalid task index");
-        
-        userTasks[msg.sender][taskIndex] = userTasks[msg.sender][userTasks[msg.sender].length - 1];
-        userTasks[msg.sender].pop();
-    }
+    function completeTask(uint256 taskID) external {
+        User storage user = users[msg.sender];
+        require(user.dailyTasks[taskID] == false, "Task already completed.");
+        user.dailyTasks[taskID] = true;
 
-    
-    function completeTask (uint256 taskID) public {
-        require(!dailyTasks[taskID], "Task already completed.");
-        dailyTasks[taskID] = true;
         emit TaskCompleted(taskID);
     }
 
-    function isTaskCompleted (uint256 taskID) public view returns(bool){
-        return dailyTasks[taskID];
+    function isTaskCompleted(uint256 taskID) public view returns (bool) {
+        return users[msg.sender].dailyTasks[taskID];
     }
 }
