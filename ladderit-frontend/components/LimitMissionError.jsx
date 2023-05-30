b249ad34158d534f4e9de40c02e460cb8e7b6729
Recep Cankaya <recepcnky@outlook.com>
2023-05-30 22:17:27 +0300

    start limit mission error

---
+++ ladderit-frontend/components/LimitMissionError.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+import styles from "../src/styles/LimitMissionError.module.css";
+
+export default function LimitMissionError() {
+  return (
+    <div className={styles.container}>You have reached the mission limit</div>
+  );
+}
--- ladderit-frontend/src/app/page.js
+++ ladderit-frontend/src/app/page.js
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import MissionForm from "../../components/MissionForm";
 import MissionList from "../../components/MissionList";
+import LimitMissionError from "../../components/LimitMissionError";
 
 export default function Home() {
   const [missions, setMissions] = useState([]);
@@ -9,6 +10,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {missions.length > 5 && <LimitMissionError />}
       <MissionForm text={text} setText={setText} setMissions={setMissions} />
       <MissionList missions={missions} />
     </main>
