f116f0bd6fb565145066032056192e2bbd4633aa
Recep Cankaya <recepcnky@outlook.com>
2023-06-03 11:04:54 +0300

    prevent submit when the input is less than 3

--- ladderit-frontend/components/MissionForm.jsx
+++ ladderit-frontend/components/MissionForm.jsx
@@ -1,10 +1,14 @@
 "use client";
+import { useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function MissionForm({ text, setText, setMissions, missions }) {
   const handleFormSubmit = (e) => {
     e.preventDefault();
+    if (text.length < 3) {
+      return handleLessText();
+    }
     setMissions((prev) => [...prev, text]);
   };
 
@@ -12,11 +16,26 @@ export default function MissionForm({ text, setText, setMissions, missions }) {
     setText(e.target.value);
   };
 
+  const handleLessText = () => {
+    if (text.length < 3) {
+      toast.error("You have to enter a mission", {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: false,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+  };
+
   const handleClick = () => {
     if (missions.length > 4) {
       toast.error("You reached the mission limit", {
         position: "top-right",
-        autoClose: 2500,
+        autoClose: 2000,
         hideProgressBar: false,
         closeOnClick: true,
         pauseOnHover: false,
