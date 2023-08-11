b641dc20634d3b02513ab3cfb8756baa62092d27
Recep Cankaya <recepcnky@outlook.com>
2023-08-11 19:03:48 +0300

    deploy contracts to mumbai

--- ladderit-hardhat/hardhat.config.js
+++ ladderit-hardhat/hardhat.config.js
@@ -7,7 +7,6 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
-  defaultNetwork: "sepolia",
   networks: {
     sepolia: {
       url: ALCHEMY_HTTP_URL,
