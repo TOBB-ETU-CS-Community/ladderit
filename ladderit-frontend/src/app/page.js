dc1d29b0eaa6086d6e5c95fd9ad52f12969a7311
Recep Cankaya <recepcnky@outlook.com>
2023-08-12 18:05:53 +0300

    migrate main page to thirdweb

--- ladderit-frontend/src/app/page.js
+++ ladderit-frontend/src/app/page.js
@@ -1,45 +1,30 @@
 "use client";
-import { useState, useContext, useEffect } from "react";
+import { useState, useEffect } from "react";
+import { useContractRead, useContract, useAdress } from "@thirdweb-dev/react";
+
 import MissionForm from "../../components/MissionForm";
 import MissionList from "../../components/MissionList";
-import { ContextAPI } from "../../context/ContextProvider";
+import { MAIN_CONTRACT_ADDRESS } from "../../constants";
 
 export default function Home() {
-  const [missions, setMissions] = useState([]);
   const [text, setText] = useState("");
-  const [username, setUsername] = useState("");
-  const { contractInstance, getProviderOrSigner, address, walletConnected } =
-    useContext(ContextAPI);
 
-  const getUsername = async () => {
-    try {
-      const provider = await getProviderOrSigner();
-      const contract = await contractInstance(provider);
-      const query = await contract.users(address);
-      const name = query[1];
-      setUsername(name);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    getUsername();
-  }, [walletConnected]);
+  const { contract } = useContract(MAIN_CONTRACT_ADDRESS);
+  const userAddress = useAdress();
+  const { data, isLoading, error } = useContractRead(
+    contract,
+    "users",
+    userAddress
+  );
 
   return (
     <>
       <h2 className="mt-8 ml-12 text-xl font-bold text-oxfordBlue underline decoration-ultraViolet">
-        Welcome {username}
+        Welcome {data}
       </h2>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <MissionForm
-          text={text}
-          setText={setText}
-          setMissions={setMissions}
-          missions={missions}
-        />
-        <MissionList missions={missions} />
+        <MissionForm text={text} setText={setText} />
+        <MissionList />
       </main>
     </>
   );
