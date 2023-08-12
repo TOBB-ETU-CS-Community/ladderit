884755464136fc1a7e30f6d24f637cabc79338e4
Recep Cankaya <recepcnky@outlook.com>
2023-08-12 18:08:14 +0300

    migrate navbar to thirdweb

--- ladderit-frontend/components/Navbar.jsx
+++ ladderit-frontend/components/Navbar.jsx
@@ -1,24 +1,15 @@
 "use client";
 import Link from "next/link";
-import { useContext } from "react";
-import { ContextAPI } from "../context/ContextProvider";
+import { ConnectWallet } from "@thirdweb-dev/react";
 
 export default function Navbar() {
-  const { address, connectWallet, walletConnected } = useContext(ContextAPI);
-
   return (
     <div className="w-full h-20 flex justify-evenly items-center bg-[#6A666C] text-textColor">
       <Link href="/" className="text-3xl">
         LADDERIT
       </Link>
       <Link href="/register">Register</Link>
-      <button
-        className="p-1.5 bg-ultraViolet text-textColor rounded-md"
-        onClick={connectWallet}>
-        {walletConnected
-          ? `${address.slice(0, 10)}...${address.slice(-7)}`
-          : "Connect Wallet"}
-      </button>
+      <ConnectWallet theme="dark" btnTitle="Connect Wallet" />
     </div>
   );
 }
